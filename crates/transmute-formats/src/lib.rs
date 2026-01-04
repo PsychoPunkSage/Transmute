a414bc07265cfc1bff55813a21e4ea90e12d25c4
Abhinav Prakash <abhinav.prakash319@gmail.com>
2026-01-04 22:02:43 +0530

    feat::> Added common formats that will be dealt with in this project
    Signed-off-by: Abhinav Prakash <abhinav.prakash319@gmail.com>

--- crates/transmute-formats/src/lib.rs
+++ crates/transmute-formats/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod decoder;
+pub mod encoder;
+pub mod metadata;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use decoder::ImageDecoder;
+pub use encoder::ImageEncoder;
+pub use metadata::ImageMetadata;
